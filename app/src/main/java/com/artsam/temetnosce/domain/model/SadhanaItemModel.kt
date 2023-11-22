3303ea2b5ded3bc6577dc8ee08189d275d938d22
Artem Samoshkin <samoshkin.job@gmail.com>
2023-11-22 23:30:55 +0200

    SadhanaScreen skeleton

--- app/src/main/java/com/artsam/temetnosce/domain/model/SadhanaItemModel.kt
+++ app/src/main/java/com/artsam/temetnosce/domain/model/SadhanaItemModel.kt
@@ -1,9 +1,11 @@
 package com.artsam.temetnosce.domain.model
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 data class SadhanaItemModel(
     val id: String,
     val label: String,
-    @DrawableRes val iconResId: Int,
+    @DrawableRes val iconResId: Int?,
+    @StringRes val timeResId: Int?,
 )
--- app/src/main/java/com/artsam/temetnosce/ui/sadhana/SadhanaScreen.kt
+++ app/src/main/java/com/artsam/temetnosce/ui/sadhana/SadhanaScreen.kt
@@ -1,21 +1,27 @@
 package com.artsam.temetnosce.ui.sadhana
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.artsam.temetnosce.R
 import com.artsam.temetnosce.domain.model.SadhanaItemModel
 
@@ -23,54 +29,82 @@ import com.artsam.temetnosce.domain.model.SadhanaItemModel
 fun SadhanaScreen() {
     // List of typical elements
     val elements = listOf(
-        SadhanaItemModel("awake", "Подъём", R.drawable.ic_sun),
-        SadhanaItemModel("service", "Служение", R.drawable.ic_feet),
-        SadhanaItemModel("kirtan", "Киртан", R.drawable.ic_musicalnote),
-        SadhanaItemModel("books", "Книги, мин", R.drawable.ic_bookopen),
-        SadhanaItemModel("lectures", "Лекции", R.drawable.ic_sun),
-        SadhanaItemModel("sleep", "Сон", R.drawable.ic_sun),
-        SadhanaItemModel("japa73", "Джапа    07:30", R.drawable.ic_sun),
-        SadhanaItemModel("japa10", "Джапа    10:00", R.drawable.ic_sun),
-        SadhanaItemModel("japa18", "Джапа    18:00", R.drawable.ic_sun),
-        SadhanaItemModel("japa24", "Джапа    24:00", R.drawable.ic_sun),
+        SadhanaItemModel("awake", "Подъём", R.drawable.ic_sun, null),
+        SadhanaItemModel("service", "Служение", R.drawable.ic_feet, null),
+        SadhanaItemModel("kirtan", "Киртан", R.drawable.ic_musicalnote, null),
+        SadhanaItemModel("books", "Книги, мин", R.drawable.ic_bookopen, null),
+        SadhanaItemModel("lectures", "Лекции", R.drawable.ic_headphones1, null),
+        SadhanaItemModel("sleep", "Сон", R.drawable.ic_moon, null),
+        SadhanaItemModel("japa73", "Джапа", null, R.string.time_0730),
+        SadhanaItemModel("japa10", "", null, R.string.time_1000),
+        SadhanaItemModel("japa18", "", null, R.string.time_1800),
+        SadhanaItemModel("japa24", "", null, R.string.time_2400),
         // Add as many elements as needed
     )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        // Header Text
+        Text(
+            text = stringResource(R.string.my_sadhana_today),
+            fontWeight = FontWeight.Bold,
+            color = Color(R.color.black),
+            modifier = Modifier.padding(bottom = 16.dp),
+            style = MaterialTheme.typography.headlineLarge.copy(fontSize = 30.sp)
+        )
 
-    LazyColumn {
-        items(elements) { item ->
-            // For each item in the list, create a horizontal composition
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                // "Label"
-                Text(
-                    text = item.label,
-                    modifier = Modifier.weight(1f)
-                )
-
-                // "Icon"
-                Icon(
-                    painter = painterResource(id = item.iconResId),
-                    contentDescription = null, // setContentDescription can be added for accessibility
-                    modifier = Modifier.padding(8.dp)
-                )
-
-                // "EditText" with border and rounded corners
-                BasicTextField(
-                    value = "", // Here can be the state of your text
-                    onValueChange = {},
+        LazyColumn {
+            items(elements) { item ->
+                // For each item in the list, create a horizontal composition
+                Row(
                     modifier = Modifier
-                        .weight(1f)
-                        .border(
-                            1.dp,
-                            Color.Gray,
-                            shape = RoundedCornerShape(8.dp)
-                        ) // 1 dp thickness, gray color, 8 dp rounded corners
-                        .padding(4.dp) // Additional padding for aesthetics
-                )
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // "Label"
+                    Text(
+                        text = item.label,
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    // "Icon" or "Text" based on the resource type
+                    item.iconResId?.let {
+                        // If it's an image resource
+                        Icon(
+                            painter = painterResource(id = it),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .size(24.dp)
+                        )
+                    }
+
+                    item.timeResId?.let {
+                        // If it's a string resource
+                        val text = stringResource(id = it)
+                        Text(
+                            text = text,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+
+                    // "EditText" with border and rounded corners
+                    BasicTextField(
+                        value = "", // Here can be the state of your text
+                        onValueChange = {},
+                        modifier = Modifier
+                            .weight(1f)
+                            .border(
+                                1.dp,
+                                Color.Gray,
+                                shape = RoundedCornerShape(8.dp)
+                            ) // 1 dp thickness, gray color, 8 dp rounded corners
+                            .padding(4.dp) // Additional padding for aesthetics
+                    )
+                }
             }
         }
     }
